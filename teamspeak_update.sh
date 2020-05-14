7d16787d0cb08d76b9b73e5c9b1b97dda3244bc6
Marcel P <32096029+plomi-net@users.noreply.github.com>
2020-05-14 10:16:43 +0200

    Update teamspeak_update.sh

--- teamspeak_update.sh
+++ teamspeak_update.sh
@@ -1,17 +1,10 @@
-
 #!/bin/bash
-#
-#######################
-# @github: https://github.com/plomi-net/server-update-script/teamspeak_update.sh
-# @version: 1.0.0
-#######################
+export LANG=de_DE.UTF-8
 
-
-#######################
-# CONFIG              #
-#######################
+# CONFIG
 
 SERVER_OS=linux
+
 SERVER_TYPE=amd64
 
 CHOWN_USER=myuser
@@ -19,9 +12,9 @@ CHOWN_GROUP=myuser
 
 SYSTEM_COMMAND=/etc/init.d/teamspeak3
 
-#######################
+###############
 # do not edit
-#######################
+###############
 
 # get latest version from teamspeak site
 VERSION=$(curl -s https://www.teamspeak.com/en/downloads/#server | grep -o 'https://files.teamspeak-services.com/releases/server/.*/teamspeak3-server_linux_amd64-.*.tar.bz2' | head -n1 | cut -d "/" -f6)
@@ -32,26 +25,25 @@ fi
 
 INSTALLED_VERSION=$(cat ts3.version)
 
-echo " "
-echo "New Version is: $VERSION"
-echo "Current Version is: $INSTALLED_VERSION"
-
 if [ $VERSION == $INSTALLED_VERSION ]; then
-        echo " "
-        echo "TS is up to date"
-        echo " "
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Teamspeak is up to date (current: $INSTALLED_VERSION)"
         exit;
 fi
 
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Download and Update Teamspeak (current: $INSTALLED_VERSION, new $VERSION)"
 LINK="https://files.teamspeak-services.com/releases/server/$VERSION/teamspeak3-server_"$SERVER_OS"_"$SERVER_TYPE"-"$VERSION".tar.bz2"
-wget $LINK -O "latest.tar.bz2"
-tar -xvf "latest.tar.bz2"
+wget $LINK -O "latest.tar.bz2" >/dev/null 2>&1
+if [ ! $? == 0 ]; then
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Error Download File"
+        exit;
+fi
+tar -xvf "latest.tar.bz2" >/dev/null 2>&1
 $SYSTEM_COMMAND stop
-tar -jcvf teamspeak3-backup-$(date '+%Y-%m-%d').tar.bz2 teamspeak3
-cp -aR "teamspeak3-server_"$SERVER_OS"_"$SERVER_TYPE/* teamspeak3
+tar -jcvf teamspeak3-backup-$(date '+%Y-%m-%d').tar.bz2 teamspeak3 >/dev/null 2>&1
+cp -aR "teamspeak3-server_"$SERVER_OS"_"$SERVER_TYPE/* teamspeak3 >/dev/null 2>&1
 chown -R $CHOWN_USER:$CHOWN_GROUP teamspeak3
 $SYSTEM_COMMAND start
-rm -rf teamspeak3-server_linux_amd64
+rm -rf "teamspeak3-server_"$SERVER_OS"_"$SERVER_TYPE
 rm -rf latest.tar.bz2
 echo $VERSION > ts3.version
-echo "Update Done!"
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Update Done!"
